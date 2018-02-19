f57b066bdef807509161062ac9809919b311555a
Ben Blaiszik <blaiszik@uchicago.edu>
2018-02-19 09:15:41 -0600

    Removing some extraneous portal features

--- portal/decorators.py
+++ portal/decorators.py
@@ -7,15 +7,18 @@ def authenticated(fn):
     @wraps(fn)
     def decorated_function(*args, **kwargs):
         if not session.get('is_authenticated'):
+
+            #Check if token is still valid
+
             return redirect(url_for('login', next=request.url))
 
         if request.path == '/logout':
             return fn(*args, **kwargs)
 
-        if (not session.get('name') or
-                not session.get('email') or
-                not session.get('institution')) and request.path != '/profile':
-            return redirect(url_for('profile', next=request.url))
+        # if (not session.get('name') or
+        #         not session.get('email') or
+        #         not session.get('institution')) and request.path != '/':
+        #     return redirect(url_for('home', next=request.url))
 
         return fn(*args, **kwargs)
     return decorated_function
