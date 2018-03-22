43b92e05c74dd3de8c9897fc55677a86d7bcf2f3
Ben Blaiszik <blaiszik@uchicago.edu>
2018-03-22 11:59:36 -0500

    Adding new bundle

--- portal/static/dist/bundle.js
+++ portal/static/dist/bundle.js
@@ -180,7 +180,10 @@
 
 	function format_form_data(data) {
 	  var moc_data = {
-	    services: [],
+	    services: {
+	      globus_publish: false,
+	      citrine: false
+	    },
 	    dc: {
 	      publicationYear: "2018",
 	      identifier: {
@@ -203,13 +206,11 @@
 	  console.log(data);
 	  if (data.services instanceof Array) {
 	    if (data.services.includes("MDF Publish")) {
-	      moc_data.services.push("globus_publish");
+	      moc_data.services.globus_publish = true;
 	    }
 	    if (data.services.includes("Citrination")) {
-	      moc_data.services.push("citrine");
+	      moc_data.services.citrination = true;
 	    }
-	  } else {
-	    moc_data.services = [];
 	  }
 
 	  // Loop through authors and format them into dc
