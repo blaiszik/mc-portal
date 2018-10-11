fdaddd25150de140ba7a3b7fa0969749be95737a
Ben Galewsky <bengal1@illinois.ed>
2018-10-11 15:36:42 -0500

    Move MDF Submission to Python form processing

--- portal/forms.py
+++ portal/forms.py
@@ -1,12 +1,31 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, HiddenField
 from wtforms.validators import DataRequired
+from mdf_connect_client.mdfcc import MDFConnectClient
+from wtforms.widgets import TextArea
 
-class BoxSubmitForm(FlaskForm):
+class DatasetSubmitForm(FlaskForm):
     auth = HiddenField('AuthKey')
     file_id = HiddenField('File ID')
-    titleInput = StringField("Title", validators=[DataRequired()])
-    name = StringField("Name", validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
+    titleInput = StringField("Title")
+    authors = HiddenField("Authors")
+    institutions = HiddenField("Institutions")
+    publication_year = StringField("Publication Year")
+    data_locations = HiddenField("Data Locations")
+    description = StringField("Description", widget=TextArea())
     submit = SubmitField('Publish')
+
+    is_explicit_data_location = True
+    form_action = "/publish/box/do_publish"
+
+    def add_dc(self, mdf: MDFConnectClient):
+        mdf.create_dc_block(
+            title=self.titleInput.data,
+            authors=self.authors.data.split("\t"),
+            affiliations=self.institutions.data.split("\t"),
+            description=self.description.data,
+            publication_year=self.publication_year.data
+        )
+
+    def add_data(self, mdf: MDFConnectClient):
+        mdf.add_data(self.data_locations.data.split("\t"))
--- portal/views.py
+++ portal/views.py
@@ -1,17 +1,19 @@
 from boxsdk import OAuth2, Client
+from datetime import date
 from flask import (abort, flash, redirect, render_template, request,
                    session, url_for)
 import requests
 import json
 from flask import jsonify
 
-from portal.forms import BoxSubmitForm
+from portal.forms import DatasetSubmitForm
 
 try:
     from urllib.parse import urlencode
 except:
     from urllib import urlencode
 
+from mdf_connect_client.mdfcc import MDFConnectClient
 from globus_sdk import RefreshTokenAuthorizer
 
 from portal import app
@@ -19,51 +21,80 @@ from portal.decorators import authenticated
 from portal.utils import (load_portal_client, get_portal_tokens,
                           get_safe_redirect)
 
-
 connect_service = "https://18.233.85.14"
+
 
 @app.route('/', methods=['GET'])
 def home():
     """Home page - play with it if you must!"""
-    #print(session['tokens'])
+    # print(session['tokens'])
     return render_template('home.jinja2')
 
-@app.route('/add', methods=['GET'])
+
+@app.route('/add', methods=['GET', 'POST'])
 @authenticated
 def add_data():
     """Route for adding data"""
-    return render_template('add_data.jinja2')
+    form = DatasetSubmitForm()
+
+    if form.validate_on_submit():
+        mdf_token = session.get("tokens").get("mdf_dataset_submission").get("refresh_token")
+        client = load_portal_client()
+        auth = RefreshTokenAuthorizer(refresh_token=mdf_token, auth_client=client)
+        mdf = MDFConnectClient(authorizer=auth, service_instance='dev')
+        form.add_dc(mdf)
+        form.add_data(mdf)
+        print("ok - we got the form back and fir example... "+str(mdf.dc))
+        print(mdf.data)
+        print("Submit status "+str(mdf.submit_dataset(test=True)))
+
+        return """<h1>Dataset submitted</h1>"""
+
+    form.authors.data = session.get('name')
+    form.publication_year.data = date.today().year
+    form.is_explicit_data_location = True
+    form.form_action = "/add"
+    return render_template('submit_dataset.jinja2', form=form)
+
 
 @app.route('/status/<source_name>', methods=['GET'])
 @authenticated
 def status(source_name):
-    headers = {"Authorization":"Bearer {}".format(session['tokens']['mdf_dataset_submission']['access_token'])}
-    r = requests.get("{connect_service}/status/{source}".format(connect_service = connect_service, 
-                                                                source=source_name),
-                        headers=headers,
-                        verify=False)
+    headers = {"Authorization": "Bearer {}".format(
+        session['tokens']['mdf_dataset_submission']['access_token'])}
+    r = requests.get("{connect_service}/status/{source}".format(
+        connect_service=connect_service,
+        source=source_name),
+                     headers=headers,
+                     verify=False)
     return render_template("status.jinja2", status=r.json())
+
 
 @app.route('/api/convert', methods=['POST'])
 def convert():
     data = json.loads(request.data)
     print(data)
-    headers = {"Authorization":"Bearer {}".format(session['tokens']['mdf_dataset_submission']['access_token'])}
+    headers = {"Authorization": "Bearer {}".format(
+        session['tokens']['mdf_dataset_submission']['access_token'])}
     print(headers)
-    r = requests.post("{connect_service}/convert/".format(connect_service = connect_service),
-                      request.data, 
-                      headers=headers, 
-                      verify=False)
+    r = requests.post(
+        "{connect_service}/convert/".format(connect_service=connect_service),
+        request.data,
+        headers=headers,
+        verify=False)
     print(r.json())
     return jsonify(r.json())
 
+
 @app.route('/api/status/<source_name>', methods=['GET'])
 def api_status(source_name):
-    headers = {"Authorization":"Bearer {}".format(session['tokens']['mdf_dataset_submission']['access_token'])}
-    r = requests.get("{connect_service}/status/{source}".format(connect_service = connect_service, 
-                                                                source=source_name),
-                        headers=headers, 
-                        verify=False)
+    headers = {"Authorization": "Bearer {}".format(
+        session['tokens']['mdf_dataset_submission']['access_token'])}
+    r = requests.get("{connect_service}/status/{source}".format(
+        connect_service=connect_service,
+        source=source_name),
+                     headers=headers,
+                     verify=False)
     return jsonify(r.json())
 
 
@@ -119,37 +150,72 @@ def logout():
 @app.route('/publish/box', methods=['POST', 'GET'])
 def publish_box():
     data = request.form
-    print(data)
 
     if not session.get('is_authenticated'):
         return redirect(
-            url_for('authcallback', next=request.url, _scheme="https",
+            url_for('authcallback', _scheme="https",
                     _external=True,
                     in_box=True,
-                    box_auth = data['auth'],
-                    box_file_id = data['file_id'],
-                    box_user_id = data['user_id']
-                    ))
+                    box_auth=data['auth'],
+                    box_file_id=data['file_id'],
+                    box_user_id=data['user_id']))
+    else:
+        return redirect(url_for('box_do_publish',
+                             _scheme="https",
+                             _external=True,
+                             box_auth=data['auth'],
+                             box_file_id=data['file_id'],
+                             box_user_id=data['user_id']))
 
+
+@app.route("/publish/box/do_publish", methods=['GET', 'POST'])
+def box_do_publish():
     oauth = OAuth2(
         client_id='61f7edsvpek2ohzfjw9ft7swde9zim2w',
         client_secret='TcoYuwIZGNEOPDIhI4uqyNophOrqWNJZ'
     )
 
-    box_auth = data.get("auth", request.args.get("box_auth"))
-    box_file_id = data.get("file_id", request.args.get("file_id"))
-    box_user_id = data.get("user_id", request.args.get("user_id"))
+    form = DatasetSubmitForm()
+
+    if form.is_submitted():
+        print("submitted")
+
+    if form.validate():
+        print("valid")
+
+    print(form.errors)
+    print("---->"+str(form.data))
+    if form.validate_on_submit():
+
+        for key in request.form:
+            print("--->"+str(key))
+
+        mdf_token = session.get("tokens").get("mdf_dataset_submission").get("refresh_token")
+        print("mdf token "+str(session.get("tokens")))
+        client = load_portal_client()
+        auth = RefreshTokenAuthorizer(refresh_token=mdf_token, auth_client=client)
+        mdf = MDFConnectClient(authorizer=auth)
+        form.add_dc(mdf)
+        print("ok - we got the form back and fir example... "+str(mdf.dc))
+        return """<h1>Box Dataset submitted</h1>"""
+
+    box_auth = request.args.get("box_auth")
+    box_file_id = request.args.get("box_file_id")
+    box_user_id = request.args.get("box_user_id")
 
     access_token, refresh_token = oauth.authenticate(box_auth)
     box_client = Client(oauth)
     user = box_client.user(box_user_id).get()
     print(user["name"])
     # file = box_client.file(box_file_id).get()
-    file=box_client.folder(box_file_id).get()
-    form = BoxSubmitForm()
-    form.name.data = user["name"]
+    file = box_client.folder(box_file_id).get()
+    form.authors.data = user["name"]
     form.titleInput.data = file["name"]
-    return render_template('submit_box.jinja2', form=form)
+    form.publication_year.data = date.today().year
+    form.is_explicit_data_location = False
+    form.form_action = "/publish/box/do_publish"
+    return render_template('submit_dataset.jinja2', form=form)
+
 
 @app.route('/authcallback', methods=['GET'])
 def authcallback():
@@ -168,8 +234,6 @@ def authcallback():
     else:
         state = {}
 
-
-
     if 'error' in request.args:
         flash("You could not be logged into the portal: " +
               request.args.get('error_description', request.args['error']))
@@ -177,21 +241,22 @@ def authcallback():
 
     # Set up our Globus Auth/OAuth2 state
     # redirect_uri = "https://connect.materialsdatafacility.org/authcallback"
-    redirect_uri = url_for('authcallback', _scheme="https",_external=True,
+    redirect_uri = url_for('authcallback', _scheme="https", _external=True,
                            in_box=in_box_integration)
 
-
-    #url_for('authcallback', _external=True)
+    # url_for('authcallback', _external=True)
 
     requested_scopes = ["openid", "profile", "email",
                         "https://auth.globus.org/scopes/c17f27bb-f200-486a-b785-2a25e82af505/connect"]
 
     client = load_portal_client()
     print(str(client))
-    print("redirect to "+redirect_uri)
-    print("scopes "+str(requested_scopes))
+    print("redirect to " + redirect_uri)
+    print("scopes " + str(requested_scopes))
     client.oauth2_start_flow(requested_scopes=requested_scopes,
-                            redirect_uri=redirect_uri, state=json.dumps(state))
+                             redirect_uri=redirect_uri,
+                             refresh_tokens=True,
+                             state=json.dumps(state))
 
     # If there's no "code" query string parameter, we're in this route
     # starting a Globus Auth login flow.
@@ -206,7 +271,7 @@ def authcallback():
         # If we do have a "code" param, we're coming back from Globus Auth
         # and can start the process of exchanging an auth code for a token.
         code = request.args.get('code')
-        print("CODE:%s:"%code)
+        print("CODE:%s:" % code)
         tokens = client.oauth2_exchange_code_for_tokens(code)
         print(tokens)
 
@@ -225,7 +290,8 @@ def authcallback():
             state = request.args.get("state")
             s = json.loads(state)
 
-            next_stop = url_for('publish_box', _scheme="https",
+            next_stop = url_for('box_do_publish',
+                                _scheme="https",
                                 _external=True,
                                 box_auth=s['auth'],
                                 box_file_id=s['file_id'],
@@ -234,5 +300,3 @@ def authcallback():
             next_stop = url_for('home', _scheme="https", _external=True)
 
         return redirect(next_stop)
-
-
